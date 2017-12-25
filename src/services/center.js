033ec84f5fe31de6f3a05f8ec2813e0e8828eb01
doulijun <doulijun@upvi.com>
2017-12-25 15:11:55 +0800

    over

--- src/services/center.js
+++
@@ -1,9 +0,0 @@
-import fetch from '../utils/fetch';
-import * as Constants from '../constants/Constants';
-
-
-// 修改企业基础信息
-export const updatebaseinfo = (data) => {
-    return fetch.post('/api/enterprise/updbaseinfo', data, {}, true);
-};
-1234
